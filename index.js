173d0de700ca84f5e5bddb4d6ce6184e9f8292d9
Tom Lodge <tlodge@gmail.com>
2020-06-17 20:28:20 +0100

    cp commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const readline = require('readline');
 
 const readInterface = readline.createInterface({
-    input: fs.createReadStream('input.gcode'),
+    input: fs.createReadStream('input2.gcode'),
     output: false,//process.stdout,
     console: false
 });
@@ -44,44 +44,54 @@ const calculateTheta = (previous, current)=>{
     const _quad = quadrant(previous, current);
     let theta = 0;
 
-    const dx =Math.abs(current[0]-previous[0]);
-    const dy =Math.abs(current[1]-previous[1]);
+    const dx = Math.abs(current[0]-previous[0]);
+    const dy = Math.abs(current[1]-previous[1]);
     
-    //
+    //NEED TO DISTINGUISH BETWEEN CURRENT ANGLE FROM 0 AND DELTA FROM LAST ANGLE, AS THEY ARE TWO DIFFERENT THINGS!
 
     switch (_quad){
         case -1:
+            //TODO: check this!!
+            theta = prevtheta;
+            prevcoords = current; 
             return 0;
 
         case 0:
-            //console.log("0--> dx:", dx , "dy", dy);
-            //console.log(previous, current);
+            
             theta = Math.atan(dx/dy)
+            console.log("quad 0" , deg(theta));
             break;
 
         case 1:
             //console.log("1--> dx:", dx , "dy", dy);
             theta = Math.PI/2 + Math.atan(dy/dx);
+            console.log("quad 1" , deg(theta));
             break;
 
         case 2:
             
             theta = Math.PI + Math.atan(dx/dy);
+            console.log("quad 2" , deg(theta));
             break;
 
         default:
             //console.log("3--->dx:", dx , "dy", dy);
             theta  = ((3*Math.PI) / 2) + Math.atan(dy/dx);
+            console.log("quad 3" , deg(theta));
     }
 
     const dir = prevtheta < theta ? 1 : -1;
-    const degrees =  deg(Math.abs(theta-prevtheta));
-    
+    const tomove =  deg(Math.abs(theta-prevtheta));
+    const currentangle = deg(theta);
 
-   
+    console.log("current angle is now", currentangle);
+    console.log("previous angle was", deg(prevtheta));
+    console.log("so to move is ", tomove);
+
     prevtheta = theta;
+
     prevcoords = current;   
-    return dir * degrees;
+    return dir * tomove;
     
     
     
@@ -99,17 +109,21 @@ const quadrant = (previous, current)=>{
     //if (dx == 0 && dy==0){
      //   return -1;
    // }
-    if (dx > 0 && dy > 0){
+   //TODO - fix whern dx =0 or dy==0
+    console.log("current", current, "previous", previous, "dx,dy", dx,dy);
+   
+    if (dx >= 0 && dy > 0){
         return 0;
     }
-    else if (dx > 0 && dy < 0){
+    else if (dx > 0 && dy <= 0){
         return 1;
     }
-    else  if (dx < 0 && dy <0){
+    else  if (dx <= 0 && dy <0){
         return 2;
     }
-    else  if (dx<0 && dy > 0) {
+    else  if (dx<0 && dy >= 0) {
         return 3;
     }
+    console.log("returning -1!!!!!");
     return -1;
 }
